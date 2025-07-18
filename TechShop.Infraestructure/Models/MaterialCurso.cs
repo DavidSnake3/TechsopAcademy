2206cee544ceca6360950e4fb829314b0cfcb2f4
J36hu4 <jeherrerasa@gmail.com>
2025-07-17 18:11:58 -0600

    Material y navbar

--- TechShop.Infraestructure/Models/MaterialCurso.cs
+++ TechShop.Infraestructure/Models/MaterialCurso.cs
@@ -5,19 +5,31 @@ namespace TechShop.Infraestructure.Models;
 
 public partial class MaterialCurso
 {
+    public List<SeccionMaterial> Secciones { get; set; } = new();
+
+    public virtual Capacitaciones Capacitacion { get; set; } = null!;
+}
+
+public class SeccionMaterial
+{
     public int Id { get; set; }
 
     public int CapacitacionId { get; set; }
 
-    public string NombreArchivo { get; set; } = null!;
+    public string Titulo { get; set; } = null!;
 
-    public string RutaArchivo { get; set; } = null!;
+    public int Posicion { get; set; }
 
-    public string TipoArchivo { get; set; } = null!;
-
-    public DateTime FechaSubida { get; set; }
-
-    public string? VideoUrl { get; set; }
-
-    public virtual Capacitaciones Capacitacion { get; set; } = null!;
+    public List<ComponenteMaterial> Componentes { get; set; } = new();
 }
+
+public class ComponenteMaterial
+{
+
+    public int Id { get; set; }
+    public int SeccionId { get; set; }
+    public string Tipo { get; set; } // "Texto", "Video", "Archivo", "Titulo"
+    public string Contenido { get; set; } // texto o URL
+    public int posicion { get; set; }
+}
+
